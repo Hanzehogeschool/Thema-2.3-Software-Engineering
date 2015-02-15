1fff2b1db4809f29b90a4e1690d468c772cd96f8
Nils Berlijn <n_berlijn@hotmail.com>
2015-02-15 17:02:03 +0100

    Javadoc added.

--- Learning task 1/Calculator/src/multiformat/Rational.java
+++ Learning task 1/Calculator/src/multiformat/Rational.java
@@ -20,137 +20,208 @@ package multiformat;
 
 /**
  * Class representing a rational ('breuk').
- * @author J.Baljé: Added comments
+ *
+ * @author Nils Berlijn
+ * @author Tom Broenink
+ * @version 1.0
  */
 public class Rational {
-	static final double PRECISION=10;
-	static final double EPSILON = Math.pow(10,-PRECISION);
-	
-	double numerator = 0.0; // teller
-	double denominator = 1.0; // noemer
 
-	/**
-	 * Create a new Rational
-	 * @param num Numerator 
-	 * @param den Denominator 
-	 */
-	public Rational(double num, double den) {
-		numerator = num;
-		denominator = den;
-		simplify();
-	}
+    static final double PRECISION = 10;
+    static final double EPSILON = Math.pow(10, -PRECISION);
 
-	public Rational() {
-	}
+    double numerator = 0.0; // teller
+    double denominator = 1.0; // noemer
 
-	public Rational(double number) {
-		numerator = number;
-		denominator = 1.0;
-		canonical();
-		simplify();
-	}
+    /**
+     * Constructor
+     * Create a new Rational
+     *
+     * @param num Numerator
+     * @param den Denominator
+     */
+    public Rational(double num, double den) {
+        numerator = num;
+        denominator = den;
+        simplify();
+    }
 
-	/**
-	 * Get rid of any decimals in the numerator. E.g. 12.5/1.0 becomes 125.0/10.0
-	 * (Note that any decimals in the denominator aren't handled, eg 10/0.5.
-	 *  This seems an omission.)
-	 *  Seen also unittest TestRational.java
-	 */
-	public void canonical() {
-		double num = Math.abs(numerator);
-		double decimal = num - Math.floor(num);
-		int num_digits = 0;
+    /**
+     * Constructor
+     * Create a new Rational
+     * Parameter-free Constructor
+     */
+    public Rational() {
 
-		while (decimal > EPSILON && num_digits < PRECISION) {
-			num = num * 10;
-			decimal = num - Math.floor(num);
-			num_digits++;
-		}
+    }
 
-		numerator = numerator * Math.pow(10.0, num_digits);
-		denominator = denominator * Math.pow(10.0, num_digits);
-	}
+    /**
+     * Constructor
+     * Create a new Rational
+     * User for double
+     *
+     * @param number Number
+     */
+    public Rational(double number) {
+        numerator = number;
+        denominator = 1.0;
+        canonical();
+        simplify();
+    }
 
-	/**
-	 * Simplify the rational. 125/10 becomes 25/2.
-	 *  Seen also unittest TestRational.java
-	 */
-	public void simplify() {
-		// Take the smallest from the two (10.0)
-		double divisor = Math.min(Math.abs(numerator), denominator);
-		// Step from 10.0 to 9.0 to ... 1.0
-		for (; divisor > 1.0; divisor -= 1.0) {
-			double rn =	Math.abs(
-					Math.IEEEremainder(Math.abs(numerator), divisor));
-			double rd = Math.abs(
-					Math.IEEEremainder(denominator, divisor));
-			// If both the numerator and denominator have a very small remainder
-			// then they can both be divided by devisor (in our example 5).
-			if (rn < EPSILON && rd < EPSILON) {
-				numerator /= divisor;
-				denominator /= divisor;
-				divisor = Math.min(Math.abs(numerator), denominator);
-			}
-		}
-	}
+    /**
+     * Get rid of any decimals in the numerator. E.g. 12.5/1.0 becomes 125.0/10.0
+     * (Note that any decimals in the denominator aren't handled, eg 10/0.5.
+     * This seems an omission.)
+     * Seen also unittest TestRational.java
+     *
+     * @see test.TestRational
+     */
+    public void canonical() {
+        double num = Math.abs(numerator);
+        double decimal = num - Math.floor(num);
+        int num_digits = 0;
 
-	/**
-	 * Add two rationals
-	 * @param other Another Rational to add to this.
-	 * @return A new Rational representing the sum.
-	 */
-	public Rational plus(Rational other) {
-		if (denominator == other.denominator)
-			return new Rational(numerator + other.numerator
-								,other.denominator);
-		else
-			// a/x + b/y = 
-			// (breuken gelijknamig maken)
-			// a*y/x*y + b*x/x*y = (a*y + b*x)/x*y
-			return new Rational(numerator * other.denominator + 
-										denominator * other.numerator
-								,denominator * other.denominator);
-	}
+        while (decimal > EPSILON && num_digits < PRECISION) {
+            num = num * 10;
+            decimal = num - Math.floor(num);
+            num_digits++;
+        }
 
-	public Rational minus(Rational other) {
-		if (denominator == other.denominator)
-			return new Rational(numerator - other.numerator, denominator);
-		else
-			return new Rational(numerator * other.denominator - 
-									denominator * other.numerator
-								,denominator * other.denominator);
-	}
+        numerator = numerator * Math.pow(10.0, num_digits);
+        denominator = denominator * Math.pow(10.0, num_digits);
+    }
 
-	public Rational mul(Rational other) {
-		return new Rational(
-			numerator * other.numerator,
-			denominator * other.denominator);
-	}
+    /**
+     * Simplify the rational. 125/10 becomes 25/2.
+     * Seen also unittest TestRational.java
+     *
+     * @see test.TestRational
+     */
+    public void simplify() {
+        // Take the smallest from the two (10.0)
+        double divisor = Math.min(Math.abs(numerator), denominator);
+        // Step from 10.0 to 9.0 to ... 1.0
+        for (; divisor > 1.0; divisor -= 1.0) {
+            double rn = Math.abs(
+                    Math.IEEEremainder(Math.abs(numerator), divisor));
+            double rd = Math.abs(
+                    Math.IEEEremainder(denominator, divisor));
+            // If both the numerator and denominator have a very small remainder
+            // then they can both be divided by devisor (in our example 5).
+            if (rn < EPSILON && rd < EPSILON) {
+                numerator /= divisor;
+                denominator /= divisor;
+                divisor = Math.min(Math.abs(numerator), denominator);
+            }
+        }
+    }
 
-	public Rational div(Rational other) {
-		return new Rational(
-			numerator * other.denominator,
-			denominator * other.numerator);
-	}
+    /**
+     * Add two rationals.
+     *
+     * @param other Another Rational to add to this.
+     * @return A new Rational representing the sum.
+     */
+    public Rational plus(Rational other) {
+        if (denominator == other.denominator)
+            return new Rational(numerator + other.numerator
+                    , other.denominator);
+        else
+            // a/x + b/y =
+            // (breuken gelijknamig maken)
+            // a*y/x*y + b*x/x*y = (a*y + b*x)/x*y
+            return new Rational(numerator * other.denominator +
+                    denominator * other.numerator
+                    , denominator * other.denominator);
+    }
 
-	public void copyOf(Rational other) {
-		this.numerator = other.numerator;
-		this.denominator = other.denominator;
-	}
-	
-	// Added getters and setters for unittesting purposes.
-	public double getNumerator(){
-		return numerator;
-	}
-	
-	public double getDenominator() {
-		return denominator;
-	}
-	public void setNumerator(double num){
-		numerator = num;
-	}
-	
-	public void setDenominator(double den) {
-		denominator = den;
-	}
+    /**
+     * Minus two Rationals.
+     *
+     * @param other Another Rational to minus to this.
+     * @return A new Rational representing the sum.
+     */
+    public Rational minus(Rational other) {
+        if (denominator == other.denominator)
+            return new Rational(numerator - other.numerator, denominator);
+        else
+            return new Rational(numerator * other.denominator -
+                    denominator * other.numerator
+                    , denominator * other.denominator);
+    }
+
+    /**
+     * Multiplies two Rationals.
+     *
+     * @param other Another Rational to multiply to this.
+     * @return A new Rational representing the sum.
+     */
+    public Rational mul(Rational other) {
+        return new Rational(
+                numerator * other.numerator,
+                denominator * other.denominator);
+    }
+
+    /**
+     * Divides two Rationals.
+     * It checks also if it's not divided by 0.
+     *
+     * @param other Another Rational to devide to this.
+     * @return A new Rational representing the sum.
+     */
+    public Rational div(Rational other) {
+        return new Rational(
+                numerator * other.denominator,
+                denominator * other.numerator);
+    }
+
+    /**
+     * Copy two Rationals.
+     *
+     * @param other Another Rational to copy to this.
+     */
+    public void copyOf(Rational other) {
+        this.numerator = other.numerator;
+        this.denominator = other.denominator;
+    }
+
+	/* getters and setters for unittesting purposes. */
+
+    /**
+     * Get the numerator.
+     *
+     * @return The numerator.
+     */
+    public double getNumerator() {
+        return numerator;
+    }
+
+    /**
+     * Set the numerator.
+     *
+     * @param num The new numerator.
+     */
+    public void setNumerator(double num) {
+        numerator = num;
+    }
+
+    /**
+     * Get the denominator.
+     *
+     * @return The denominator.
+     */
+    public double getDenominator() {
+        return denominator;
+    }
+
+    /**
+     * Set the denominator.
+     *
+     * @param den The new denominator.
+     */
+    public void setDenominator(double den) {
+        denominator = den;
+    }
+
 }
