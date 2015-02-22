611b0211f23c1a2ebdde031ae47e9eb2691d9bda
Nils Berlijn <n_berlijn@hotmail.com>
2015-02-23 00:08:10 +0100

    Comments removed.

--- Learning task 2/Classifier/src/gui/TreeGUI.java
+++ Learning task 2/Classifier/src/gui/TreeGUI.java
@@ -13,7 +13,6 @@ public class TreeGUI extends JFrame {
 
     public TreeGUI(ClassifierModel classifierModel) throws Exception {
         this.classifierModel = classifierModel;
-        //classifierModel = new ClassifierModel(new File(Config.CATEGORIES_FILE), new File(Config.FEATURES_FILE), new File(Config.TRAINING_SET));
 
         treeView = new TreeView(classifierModel);
         treeView.tree();
